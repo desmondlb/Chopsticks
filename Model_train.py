1d28c1876195f07d4257831576195575f0f2e35f
desmondlb <69417904+desmondlb@users.noreply.github.com>
2020-08-09 16:35:10 +0530

    Minor Tweaks
    Use model.fit() instead of model.fit_generator()

--- Model_train.py
+++ Model_train.py
@@ -12,7 +12,7 @@ def get_train_test(train_path, test_path, target):
     traindata = trdata.flow_from_directory(directory=train_path, target_size=(target, target), shuffle=True)
 
     tsdata = ImageDataGenerator()
-    testdata = tsdata.flow_from_directory(directory=test_path, target_size=(224, 224), shuffle=True)
+    testdata = tsdata.flow_from_directory(directory=test_path, target_size=(target, target), shuffle=True)
 
     return traindata, testdata
 
@@ -33,9 +33,9 @@ def train(model, traindata, testdata):
     checkpoint = ModelCheckpoint("Resnet18_1.h5", monitor='val_accuracy', verbose=1,
                                  save_best_only=True, save_weights_only=False, mode='auto', save_freq='epoch')
     early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=30, verbose=1, mode='auto')
-    model.fit_generator(generator=traindata, steps_per_epoch=5, epochs=100,
-                        validation_data=testdata, validation_steps=1, callbacks=[checkpoint, early])
-    model.save_weights("Resnet18_1.h5.h5")
+    model.fit(traindata, steps_per_epoch=5, epochs=100,
+              validation_data=testdata, validation_steps=1, callbacks=[checkpoint, early])
+    model.save_weights("Resnet18_1.h5")
 
 
 def load_base_model():
@@ -51,6 +51,6 @@ traindata, testdata = get_train_test(train_path, test_path, image_inp_size)
 
 base_model = load_base_model()
 dense_layers = [1024, 256]
-model = add_custom_layers(base_model, dense_layers, activation='softmax', base_trainable = False)
+model = add_custom_layers(base_model, dense_layers, activation='softmax', base_trainable=False)
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
 train(model, traindata, testdata)
