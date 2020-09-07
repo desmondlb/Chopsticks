cb767da5b757b652f180035ec80659552e01a21b
desmondlb <69417904+desmondlb@users.noreply.github.com>
2020-09-07 18:51:51 +0530

    :wrench: Minor Tweaks

--- frame_extractor.py
+++ frame_extractor.py
@@ -2,13 +2,13 @@ import cv2
 
 # cap = cv2.VideoCapture('WIN_20200807_14_38_46_Pro.mp4')
 # cap = cv2.VideoCapture('WIN_20200807_14_39_17_Pro.mp4')
-cap = cv2.VideoCapture('D:/Projects/Chopsticks/Datasets/WIN_20200809_22_59_55_Pro.mp4')
+cap = cv2.VideoCapture('D:/Projects/Chopsticks/Datasets/4/WIN_20200825_18_56_00_Pro.mp4')
 i = 0
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
-    cv2.imwrite('D:/Projects/Chopsticks/Datasets/HandDetect/train/0/F_' + str(i) + '.jpg', frame)
+    cv2.imwrite('D:/Projects/Chopsticks/Datasets/4/fourr_' + str(i) + '.jpg', frame)
     i += 1
 
 cap.release()
